684a34856fa4b2a38e9c24e1fe3c81497b8ec8ea
Rao Burugular <burugular@gmail.com>
2021-09-12 14:53:55 +0100

    Updated unit tests

--- app/src/test/java/com/example/dogapi/data/DataRepositoryTest.kt
+++ app/src/test/java/com/example/dogapi/data/DataRepositoryTest.kt
@@ -1,14 +1,11 @@
-package com.example.dogapi
+package com.example.dogapi.data
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.dogapi.api.ApiServiceInterface
-import com.example.dogapi.data.DataRepository
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Rule
 import org.junit.Test
 
@@ -18,15 +15,12 @@ class DataRepositoryTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private val testDispatcher = TestCoroutineDispatcher()
-    private val testCoroutineScope = TestCoroutineScope(testDispatcher)
-
     private val apiServiceInterface: ApiServiceInterface = mockk()
     private val dataRepository = DataRepository(apiServiceInterface)
 
     @Test
     fun fetchCatBreeds() {
-        testCoroutineScope.launch {
+        runBlockingTest {
             dataRepository.getCatBreeds()
             coVerify { apiServiceInterface.getCatBreeds() }
         }
--- app/src/test/java/com/example/dogapi/viewModel/MainActivityViewModelTest.kt
+++ app/src/test/java/com/example/dogapi/viewModel/MainActivityViewModelTest.kt
@@ -1,18 +1,14 @@
-package com.example.dogapi
+package com.example.dogapi.viewModel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import com.example.dogapi.data.DataRepository
 import com.example.dogapi.model.CatBreed
 import com.example.dogapi.model.CatImage
-import com.example.dogapi.viewModel.MainActivityViewModel
 import io.mockk.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.runBlockingTest
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
@@ -23,36 +19,26 @@ class MainActivityViewModelTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private val testDispatcher = TestCoroutineDispatcher()
-    private val testCoroutineScope = TestCoroutineScope(testDispatcher)
-
     private val dataRepository: DataRepository = mockk(relaxed = true)
-    private val mainActivityViewModel = MainActivityViewModel(dataRepository)
+    private val viewModel = MainActivityViewModel(dataRepository)
     private var observer: Observer<Result<List<CatBreed>>> = mockk(relaxed = true)
-
-    @Test
-    fun fetchCatBreeds() {
-        testCoroutineScope.launch {
-            mainActivityViewModel.fetchCatBreeds()
-            coVerify { dataRepository.getCatBreeds() }
-        }
-    }
 
     @Test
     fun `WHEN fetchCatBreeds is successful THEN call once the corresponding repository and the success data`() {
         coEvery { dataRepository.getCatBreeds() } returns Result.success(buildFakeCatBreeds())
-        mainActivityViewModel.catBreedsLiveData.observeForever { observer }
+        viewModel.catBreedsLiveData.observeForever(observer)
 
-        testCoroutineScope.launch {
-            mainActivityViewModel.fetchCatBreeds()
+        runBlockingTest {
+            viewModel.fetchCatBreeds()
 
             coVerify(exactly = 1) { dataRepository.getCatBreeds() }
 
             val slot = slot<Result<ArrayList<CatBreed>>>()
             verify { observer.onChanged(capture(slot)) }
             assertTrue(slot.captured.isSuccess)
+            assertEquals(slot.captured.getOrNull(), buildFakeCatBreeds())
 
-            mainActivityViewModel.catBreedsLiveData.removeObserver(observer)
+            viewModel.catBreedsLiveData.removeObserver(observer)
         }
     }
 
