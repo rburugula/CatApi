6e7d0228bc5ffa6eb7d92ab94be5fda691ad849b
Rao Burugular <burugular@gmail.com>
2021-09-12 01:00:21 +0100

    added unit tests for live data in view model

--- app/src/test/java/com/example/dogapi/MainActivityViewModelTest.kt
+++ app/src/test/java/com/example/dogapi/MainActivityViewModelTest.kt
@@ -1,14 +1,19 @@
 package com.example.dogapi
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import com.example.dogapi.data.DataRepository
+import com.example.dogapi.model.CatBreed
+import com.example.dogapi.model.CatImage
 import com.example.dogapi.viewModel.MainActivityViewModel
-import io.mockk.coVerify
-import io.mockk.mockk
+import io.mockk.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 
@@ -23,6 +28,7 @@ class MainActivityViewModelTest {
 
     private val dataRepository: DataRepository = mockk(relaxed = true)
     private val mainActivityViewModel = MainActivityViewModel(dataRepository)
+    private var observer: Observer<Result<List<CatBreed>>> = mockk(relaxed = true)
 
     @Test
     fun fetchCatBreeds() {
@@ -31,4 +37,36 @@ class MainActivityViewModelTest {
             coVerify { dataRepository.getCatBreeds() }
         }
     }
+
+    @Test
+    fun `WHEN fetchCatBreeds is successful THEN call once the corresponding repository and the success data`() {
+        coEvery { dataRepository.getCatBreeds() } returns Result.success(buildFakeCatBreeds())
+        mainActivityViewModel.catBreedsLiveData.observeForever { observer }
+
+        testCoroutineScope.launch {
+            mainActivityViewModel.fetchCatBreeds()
+
+            coVerify(exactly = 1) { dataRepository.getCatBreeds() }
+
+            val slot = slot<Result<ArrayList<CatBreed>>>()
+            verify { observer.onChanged(capture(slot)) }
+            assertTrue(slot.captured.isSuccess)
+
+            mainActivityViewModel.catBreedsLiveData.removeObserver(observer)
+        }
+    }
+
+    private fun buildFakeCatBreeds() = arrayListOf(
+        CatBreed(
+            name = "myName",
+            image = CatImage(
+                url = "url",
+                height = 10,
+                width = 10
+            ),
+            temperament = "temprerament",
+            wikipedia_url = "wikiurl",
+            energy_level = 2
+        )
+    )
 }
